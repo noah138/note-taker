2247e5e8393fa8e218d7bca194591ab5183a8771
noah138 <noahschwartz@live.com>
2022-10-28 17:02:51 -0700

    added code to start the server in index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+// express boilerplate
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.send('this is a homepage')
-})
+app.use(express.static("public"))
 
-app.listen(PORT,()=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes to created route files
+require('./routes/api');
+require('./routes/html');
+
+// starts the server
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
